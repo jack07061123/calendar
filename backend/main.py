969750f58bdc94b0f29541febeb1b5d4db425fa0
JackLin <jack070613@yahoo.com.tw>
2024-09-17 22:24:32 +0800

    init calendar backend and front end

---
+++ backend/main.py
@@ -0,0 +1,38 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials as SAC
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel
+from key import sheet_key
+
+Json = "calendar.json"
+Url = ["https://spreadsheets.google.com/feeds"]
+Connect = SAC.from_json_keyfile_name(Json, Url)
+GoogleSheets = gspread.authorize(Connect)
+Sheet = GoogleSheets.open_by_key(sheet_key)
+Sheets = Sheet.sheet1
+
+
+##########Insert Data Into Database
+# dataTitle = ['date', 'time', 'event', 'place']
+# datas = ["03/11", "15:00", "dinner", "taipei"]
+# #先塞一筆假資料
+# Sheets.append_row(dataTitle)
+# Sheets.append_row(datas)
+# print("寫入成功")
+# print(Sheets.get_all_values())
+
+
+# ----- fast api -----
+app = FastAPI()
+@app.get("/")
+def getAllData():
+    return Sheets.get_all_records()
+
+class Info(BaseModel):
+    id: int
+    data: list
+@app.post("/addNewEvents")
+def getInformation(info: Info):
+    Sheets.append_row(info.data)
+    return {"status": "SUCCESS", "data": info}
