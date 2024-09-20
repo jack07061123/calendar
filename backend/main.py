dcb029c35535c91df15921ec83d3fabc37921445
JackLin <jack070613@yahoo.com.tw>
2024-09-20 23:51:58 +0800

    add gitignore and .env file

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,6 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 from key import sheet_key
-
 Json = "calendar.json"
 Url = ["https://spreadsheets.google.com/feeds"]
 Connect = SAC.from_json_keyfile_name(Json, Url)
@@ -25,6 +24,20 @@ Sheets = Sheet.sheet1
 
 # ----- fast api -----
 app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def getAllData():
     return Sheets.get_all_records()
