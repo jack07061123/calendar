9edde004bcb3c03eb8b8e91645fee520a546b1d0
JackLin <jack070613@yahoo.com.tw>
2024-09-21 00:25:57 +0800

    add import env

--- backend/main.py
+++ backend/main.py
@@ -10,9 +10,28 @@ from dotenv import load_dotenv
 load_dotenv()
 sheetKey = os.environ.get("sheetKey")
 
-Json = "calendar.json"
+
+def create_keyfile_dict():
+    variables_keys = {
+        "type": os.environ.get("SHEET_TYPE"),
+        "project_id": os.environ.get("SHEET_PROJECT_ID"),
+        "private_key_id": os.environ.get("SHEET_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("SHEET_PRIVATE_KEY"),
+        "client_email": os.environ.get("SHEET_CLIENT_EMAIL"),
+        "client_id": os.environ.get("SHEET_CLIENT_ID"),
+        "auth_uri": os.environ.get("SHEET_AUTH_URI"),
+        "token_uri": os.environ.get("SHEET_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("SHEET_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.environ.get("SHEET_CLIENT_X509_CERT_URL")
+
+        "universe_domain": os.environ.get("SHEET_UNIVERSE_DOMAIN")
+    }
+    return variables_keys
+
+
+# Json = "calendar.json"
 Url = ["https://spreadsheets.google.com/feeds"]
-Connect = SAC.from_json_keyfile_name(Json, Url)
+Connect = SAC.from_json_keyfile_name(create_keyfile_dict(), Url)
 GoogleSheets = gspread.authorize(Connect)
 Sheet = GoogleSheets.open_by_key(sheet_key)
 Sheets = Sheet.sheet1
